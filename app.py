d1117a256a0eb4f7675d75acf07d05d05ea55e75
Md Liakot Hossian Shuvo <128778861+liakothossian@users.noreply.github.com>
2023-07-13 16:27:28 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 from flask import Flask, render_template, request, redirect, flash, send_from_directory
 import pandas as pd
 import requests
@@ -7,8 +8,10 @@ from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 import warnings
+import uuid
+from pathlib import Path
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = './'
+app.config['UPLOAD_FOLDER'] = str(Path.home())  # Set upload folder to user's home directory
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ['xlsx', 'xls']
 @app.route('/', methods=['GET', 'POST'])
@@ -28,38 +31,49 @@ def myform():
             responsess = []
             mail_validation = {}
             ua = UserAgent()
-            for i, row in tqdm(df.iterrows(), total=len(df)):
-                email = str(row['DirectEmail'])
-                link = row['Source']
-                if pd.isna(email) or pd.isna(link):
-                    mail_validation[i] = 0
-                    responsess.append('')
-                    continue
-                headers = {'User-Agent': ua.random}
-                response = requests.get(link, headers=headers)
-                try:
-                    response.raise_for_status()
-                    if response.status_code == 404:
+            with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+                futures = []
+                for i, row in df.iterrows():
+                    email = str(row['DirectEmail'])
+                    link = row['Source']
+                    if pd.isna(email) or pd.isna(link):
                         mail_validation[i] = 0
-                    else:
-                        with warnings.catch_warnings():
-                            warnings.filterwarnings("ignore", category=Warning)
-                            soup = BeautifulSoup(response.content, 'html5lib')
-                        text = soup.get_text()
-                        if '@' in text:
-                            mail_validation[i] = 1
+                        responsess.append('')
+                        continue
+                    headers = {'User-Agent': ua.random}
+                    futures.append(executor.submit(requests.get, link, headers=headers))
+                for i, future in enumerate(tqdm(futures)):
+                    try:
+                        response = future.result()
+                        if isinstance(response, requests.Response):
+                            if response.status_code == 404:
+                                mail_validation[i] = 0
+                            else:
+                                with warnings.catch_warnings():
+                                    warnings.filterwarnings("ignore", category=Warning)
+                                    soup = BeautifulSoup(response.content, 'html5lib')
+                                text = soup.get_text()
+                                if '@' in text:
+                                    mail_validation[i] = 1
+                                else:
+                                    mail_validation[i] = 0
+                            responsess.append(response)
+                        elif isinstance(response, str):
+                            mail_validation[i] = -1
+                            responsess.append("Request Error")
                         else:
                             mail_validation[i] = 0
-                    responsess.append(response)
-                except requests.exceptions.RequestException:
-                    mail_validation[i] = -1
-                    responsess.append("Request Error")
-
+                            responsess.append('')
+                    except requests.exceptions.RequestException:
+                        mail_validation[i] = -1
+                        responsess.append("Request Error")
             df["valid_email"] = pd.Series(mail_validation)
             df["Response_Type"] = pd.Series(responsess)
-            filename1 = 'Outputfile.xlsx'
-            df.to_excel(filename1)
+            filename1 = str(uuid.uuid4()) + '.xlsx'
+            output_path = os.path.join(app.config['UPLOAD_FOLDER'], filename1)
+            df.to_excel(output_path)
             summary = {
+                "filename": filename1,
                 "total": len(df),
                 "valid_emails": len(df.loc[df['valid_email'] == 1]),
                 "invalid_emails": len(df.loc[df['valid_email'] == 0]),
@@ -67,10 +81,8 @@ def myform():
             }
             return render_template('summary.html', summary=summary)
     return render_template('index.html')
-
-@app.route('/download')
-def download_file():
-    return send_from_directory(app.config['UPLOAD_FOLDER'], 'Outputfile.xlsx', as_attachment=True)
-
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
 if __name__ == '__main__':
     app.run(port=1234, debug=True)
