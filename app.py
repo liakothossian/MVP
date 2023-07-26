ccf0e1839b9d12173a5810920a76ceb4b2b2942a
Md Liakot Hossian Shuvo <128778861+liakothossian@users.noreply.github.com>
2023-07-26 15:22:44 +0600

    Update app.py

--- app.py
+++ app.py
@@ -10,79 +10,117 @@ from tqdm import tqdm
 import warnings
 import uuid
 from pathlib import Path
+from datetime import datetime
+
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = str(Path.home())  # Set upload folder to user's home directory
+app.config['UPLOAD_FOLDER'] = str(
+  Path.home())  # Set upload folder to the user's home directory
+
+
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ['xlsx', 'xls']
+  return '.' in filename and filename.rsplit('.',
+                                             1)[1].lower() in ['xlsx', 'xls']
+
+
 @app.route('/', methods=['GET', 'POST'])
 def myform():
-    if request.method == 'POST':
-        if 'x' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['x']
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            df = pd.read_excel(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            responsess = []
-            mail_validation = {}
-            ua = UserAgent()
-            with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-                futures = []
-                for i, row in df.iterrows():
-                    email = str(row['DirectEmail'])
-                    link = row['Source']
-                    if pd.isna(email) or pd.isna(link):
-                        mail_validation[i] = 0
-                        responsess.append('')
-                        continue
-                    headers = {'User-Agent': ua.random}
-                    futures.append(executor.submit(requests.get, link, headers=headers))
-                for i, future in enumerate(tqdm(futures)):
-                    try:
-                        response = future.result()
-                        if isinstance(response, requests.Response):
-                            if response.status_code == 404:
-                                mail_validation[i] = 0
-                            else:
-                                with warnings.catch_warnings():
-                                    warnings.filterwarnings("ignore", category=Warning)
-                                    soup = BeautifulSoup(response.content, 'html5lib')
-                                text = soup.get_text()
-                                if '@' in text:
-                                    mail_validation[i] = 1
-                                else:
-                                    mail_validation[i] = 0
-                            responsess.append(response)
-                        elif isinstance(response, str):
-                            mail_validation[i] = -1
-                            responsess.append("Request Error")
-                        else:
-                            mail_validation[i] = 0
-                            responsess.append('')
-                    except requests.exceptions.RequestException:
-                        mail_validation[i] = -1
-                        responsess.append("Request Error")
-            df["valid_email"] = pd.Series(mail_validation)
-            df["Response_Type"] = pd.Series(responsess)
-            filename1 = str(uuid.uuid4()) + '.xlsx'
-            output_path = os.path.join(app.config['UPLOAD_FOLDER'], filename1)
-            df.to_excel(output_path)
-            summary = {
-                "filename": filename1,
-                "total": len(df),
-                "valid_emails": len(df.loc[df['valid_email'] == 1]),
-                "invalid_emails": len(df.loc[df['valid_email'] == 0]),
-                "request_errors": len(df.loc[df['valid_email'] == -1]),
-            }
-            return render_template('summary.html', summary=summary)
-    return render_template('index.html')
+  if request.method == 'POST':
+    if 'x' not in request.files:
+      flash('No file part')
+      return redirect(request.url)
+    file = request.files['x']
+    if file.filename == '':
+      flash('No selected file')
+      return redirect(request.url)
+    if file and allowed_file(file.filename):
+      filename = secure_filename(file.filename)
+      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      df = pd.read_excel(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      responsess = []
+      mail_validation = {}
+      ua = UserAgent()
+      with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        futures = []
+        for i, row in df.iterrows():
+          email = str(row['DirectEmail'])
+          link = row['Source']
+          if pd.isna(email) or pd.isna(link):
+            mail_validation[i] = 0
+            responsess.append('')
+            continue
+          headers = {'User-Agent': ua.random}
+          futures.append(executor.submit(requests.get, link, headers=headers))
+        for i, future in enumerate(tqdm(futures)):
+          try:
+            response = future.result()
+            if isinstance(response, requests.Response):
+              if response.status_code == 404:
+                mail_validation[i] = 0
+              else:
+                with warnings.catch_warnings():
+                  warnings.filterwarnings("ignore", category=Warning)
+                  soup = BeautifulSoup(response.content, 'html5lib')
+                text = soup.get_text()
+                if '@' in text:
+                  mail_validation[i] = 1
+                else:
+                  mail_validation[i] = 0
+              responsess.append(response)
+            elif isinstance(response, str):
+              mail_validation[i] = -1
+              responsess.append("Request Error")
+            else:
+              mail_validation[i] = 0
+              responsess.append('')
+          except requests.exceptions.RequestException:
+            mail_validation[i] = -1
+            responsess.append("Request Error")
+
+      df["valid_email"] = pd.Series(mail_validation)
+      df["Response_Type"] = pd.Series(responsess)
+
+      # Create a new Excel file for writing the summary sheet
+      summary_filename = str(uuid.uuid4()) + '_summary.xlsx'
+      summary_output_path = os.path.join(app.config['UPLOAD_FOLDER'],
+                                         summary_filename)
+
+      # Write the original DataFrame to the first sheet of the summary Excel file
+      with pd.ExcelWriter(summary_output_path, engine='openpyxl') as writer:
+        df.to_excel(writer, sheet_name='Data', index=False)
+
+        # Create a summary DataFrame with the required summary information
+        summary_df = pd.DataFrame(
+          [["Total Data", len(df)],
+           ["Email Validated",
+            len(df.loc[df['valid_email'] == 1])],
+           ["Email Couldn't Validated",
+            len(df.loc[df['valid_email'] == 0])],
+           ["No of Errors",
+            len(df.loc[df['valid_email'] == -1])],
+           ["Time", datetime.now().strftime("%H:%M:%S")]
+           ],  # Updated format to show only time
+          columns=["Summary", "Value"])
+
+        # Write the summary DataFrame to a new sheet called "Summary"
+        summary_df.to_excel(writer, sheet_name='Summary', index=False)
+
+      summary = {
+        "filename": summary_filename,
+        "total": len(df),
+        "valid_emails": len(df.loc[df['valid_email'] == 1]),
+        "invalid_emails": len(df.loc[df['valid_email'] == 0]),
+        "request_errors": len(df.loc[df['valid_email'] == -1]),
+      }
+      return render_template('summary.html', summary=summary)
+  return render_template('index.html')
+
+
 @app.route('/download/<filename>')
 def download_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+  return send_from_directory(app.config['UPLOAD_FOLDER'],
+                             filename,
+                             as_attachment=True)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+  app.run(host='0.0.0.0', port=5000)
